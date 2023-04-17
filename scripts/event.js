5bf44ab7210ea78baaab413ce23b531047c55bfb
arrrmann <arman.arustamyan99@gmail.com>
2023-04-18 02:51:51 +0400

    onchange event with username input

--- scripts/event.js
+++ scripts/event.js
@@ -1,3 +1,12 @@
-function printhello(){
-    console.log("hello")
+// function printhello(){
+//     console.log("hello")
+// }
+
+function validate(){
+    const {value}=document.getElementById("username")
+    if(value.length > 5){
+        console.log(`ur name will be ${value}`)
+    }else{
+        console.log("invalid value")
+    }
 }
